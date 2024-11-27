20b026072bace4f933105a2f2c6314e0e59d62e5
Jonatan Koroch de Aguiar <59874556+koroch@users.noreply.github.com>
2024-11-27 20:48:20 -0300

    Feat/criando-testes-customers

--- api-project/src/customers/customers.service.spec.ts
+++ api-project/src/customers/customers.service.spec.ts
@@ -16,4 +16,76 @@ describe('CustomersService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  let resultCreate;
+  it('should create a customer', async () => {
+    const createCustomerDto = {
+      cpfCnpj: '68418415061',
+      password: 'securepassword123',
+      name: 'Joao Feijao',
+      email: 'john.doe@example.com',
+      addresses: [
+        {
+          address: '123 Main St',
+          number: '101',
+          complement: 'Apt 4'
+        },
+        {
+          address: '456 Second Ave',
+          number: '202',
+          complement: 'Suite B'
+        }
+      ]
+    };
+    resultCreate = await service.create(createCustomerDto);
+    expect(resultCreate).toHaveProperty('id');
+  });
+
+  it('should find all customer', async () => {
+    const result = await service.findAll();
+    expect(result).toBeInstanceOf(Array);
+    expect(result).not.toHaveLength(0);
+  });
+
+  it('should find a customer by ID', async () => {
+    const result = await service.findOne(resultCreate.id);
+    expect(result).toHaveProperty('id');
+  });
+
+  it('should update a customer', async () => {
+    const updateCustomerDto = { name: 'João Calabreso' };
+    const result = await service.update(resultCreate.id, updateCustomerDto);
+    expect(result).toHaveProperty('id');
+    expect(result.name).toBe(updateCustomerDto.name);
+  });
+
+  // it('should throw error on create with invalid data', async () => {
+  //   const createCustomerDto = {
+  //     cpfCnpj: '',
+  //     password: '',
+  //     name: 'Joao Feijao',
+  //     email: 'john.doe@example.com',
+  //     addresses: [
+  //       {
+  //         address: '123 Main St',
+  //         number: '101',
+  //         complement: 'Apt 4'
+  //       },
+  //       {
+  //         address: '456 Second Ave',
+  //         number: '202',
+  //         complement: 'Suite B'
+  //       }
+  //     ]
+  //   };
+
+  //   await expect(service.create(createCustomerDto)).rejects.toThrow(
+  //     'Name cannot be empty'
+  //   );
+  // });
+
+  // it('should remove a customer', async () => {
+  //   const result = await service.remove(resultCreate.id);
+  //   expect(result).toHaveProperty('id');
+  // });
 });
